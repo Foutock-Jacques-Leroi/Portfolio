c6621b02a2c3b19978df189c52587d4e0f44de58
Foutock-Jacques-Leroi <foutockymgajacquesleroi@gmail.com>
2025-03-21 13:28:28 +0100

    Update AboutMe.jsx

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -18,8 +18,8 @@ function AboutMe() {
                 <div className='m-4 text-center' >
                     <h1 className='md:text-6xl sm:text-5xl text-4xl sp font-extrabold bg-clip-text bg-gradient-to-l from-white to-green-400 text-transparent'>Introducing </h1>
                     <h1 className=' md:text-6xl mb-4 sm:text-5xl text-4xl sp font-extrabold bg-clip-text bg-gradient-to-l from-white to-teal-200 text-transparent'>Myself</h1>
-                    <p className='text-wrap px-4 md:text-xl sm:text-xl text-lg '><span>I</span> am a smart working guy who is in love with comprehensive and open minded ideas on intresting
-                        critical thinking and thoughts. Also my hobby is reading or learning new domain, with that i play football in parallel.
+                    <p className='text-wrap px-4 md:text-xl sm:text-xl text-lg '><span>I</span> am a Level 3 Software Engineer at IUT of Douala (Ndogbong) who is in love with comprehensive and open minded ideas on intresting
+                        critical thinking and thoughts. Also, my Hobbies are reading and learning new domain along with football because i really love the game.
                     </p>
                 </div>
                 <center><img className='rounded-xl shadow-md shadow-teal-300' src="5.jpg" alt="/" height={300} width={300}/></center>
